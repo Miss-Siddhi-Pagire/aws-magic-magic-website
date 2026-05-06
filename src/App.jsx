d616bb3f599b92fa3fd183245806b3be896e1553
Miss-Siddhi-Pagire <siddhipagire825@gmail.com>
2026-05-07 00:57:53 +0530

    Basic file is created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Magic Magic</h1>
+      <p>This is my first website on S3</p>
+    </div>
+  )
+}
+
+export default App
